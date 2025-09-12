b27bf402cb0c6b7d378c9ce0b04e74805399f186
lowhanfish <lowhanfish@gmail.com>
2025-09-12 21:56:32 +0800

    added 1-5 to lat1

--- lat1/5_fungsi.py
+++ lat1/5_fungsi.py
@@ -1,9 +1,15 @@
 """
-Sama halnya dengan pemrograman lain
+Sama halnya dengan pemrograman lain, fungsi di python dapat di simpan dalam variabel, 
+di kirim sebagai argumen, dan di kembalikan dari fungsi lain.
+
+selain itu fungsi di python di gunakan untuk mengelompokan kode program yang 
+bisa di panggil berulang kali.
+
+contoh:
 """
 
-def fungsi(kiken):
-    print("Hy nama saya ", kiken)
+def fungsi(nama):
+    print("Hy nama saya ", nama)
 
 fungsi("koko")
 
---
+++ lat1/6_operator.py
@@ -0,0 +1,34 @@
+"""
+Operator aritmatika pada python sama halnya dengan bahasa pemrograman lain, yaitu operator yang digunakan
+untuk melakukan operasi matematika pada nilai numerik.
+
+Operator aritmatika pada python:
+** pangkat, contoh 2**3 = 8
+* perkalian, contoh 2*3 = 6
+/ pembagian, contoh 6/3 = 2
+// pembagian bulat, contoh 6//4 = 1
+% sisa bagi, contoh 6%4 = 2
++ penjumlahan, contoh 2+3 = 5
+- pengurangan, contoh 5-2 = 3
+
+"""
+
+# ===========================================================
+
+a = 2**3
+b = 2*3
+c = 6/3
+d = 6//4
+e = 6%4
+f = 2+3
+g = 5-2
+
+print("2 pangkat 3 = ", a)
+print("2 kali 3 = ", b)
+print("6 bagi 3 = ", c)
+print("6 bagi bulat 4 = ", d)
+print("6 sisa bagi 4 = ", e)
+print("2 tambah 3 = ", f)
+print("5 kurang 2 = ", g)   
+
+
