97217eb3f9a2ec43a40307d1b5c88bd6e43ff9cb
lowhanfish <lowhanfish@gmail.com>
2026-02-26 23:39:31 +0800

    list_comprehension

--- lat2/8dict.py
+++ lat2/8dict.py
@@ -22,3 +22,32 @@ print(animals.get('j',"Tidak di temukan"))
 
 for data in a:
     print(data)
+
+
+
+rumah = {
+    "t36" : 150000,
+    "t45" : 350000,
+    "t72" : 450000,
+    "t100" : [
+        {
+            "type" : 'Cash', 
+            "price" : 950000, 
+        },
+        {
+            "type" : 'Credit', 
+            "price" : 1300000, 
+        },
+    ],
+}
+
+
+print(rumah)
+
+
+bb = rumah["t100"]
+
+
+
+for i, data in enumerate(bb):
+    print(f"Untuk penjualan {data['type']} harganya adalah {data['price']}")
---
+++ lat2/9listcomprehension.py
@@ -0,0 +1,32 @@
+
+
+
+
+data1 = list(range(50))
+# print("data awal : ", data1)
+
+filtering = [ data for data in data1 if data%2==0]
+# print(f"Filtering", filtering)
+
+condition = [f"{data}=Genap" if data%2==0 else f"{data}=Ganjil" for data in data1]
+# print(f"Filtering", condition)
+
+nama = "Hy, nama saya adalah Kiken S Batara. Saya adalah seorang programmer"
+# print(nama)
+
+nama_split = nama.split()
+# print(nama_split)
+nama_clear = nama.replace(".","").lower()
+# print(nama_clear)
+
+def clear_text (txt):
+    gen = txt.replace(".","").replace(",",'').lower()
+    # gen = txt.replace(",","").lower()
+    return gen
+
+coba1 = [clear_text(data) for data in nama.split()]
+print (coba1)
+
+
+
+
